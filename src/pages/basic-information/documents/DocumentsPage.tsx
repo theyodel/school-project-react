b4eabd1d932d11cdbb12aa6c5ba9a87183985ad0
youngyanachlene <skobozabr24@gmail.com>
2024-02-03 02:00:01 +0300

    test

--- src/pages/basic-information/documents/DocumentsPage.tsx
+++ src/pages/basic-information/documents/DocumentsPage.tsx
@@ -1,5 +1,5 @@
 import Footer from "../../../components/Footer";
-import Navbar from "../../../components/Navbar";
+import Navbar from "../../../components/Navbaa";
 
 function DocumentsPage() {
   return (
